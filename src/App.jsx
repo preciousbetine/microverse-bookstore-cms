e1b01cf731a8e99b5a61234cc9139e8fdcf7dda8
Precious Betine <preciousbetine@gmail.com>
2023-03-30 13:15:36 +0100

    Finish site design

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+import Home from '@/components/Home';
+import Categories from '@/components/Categories';
+import Layout from '@/components/Layout';
+import '@/styles/App.scss';
+
 const App = () => (
-  <div className="App" />
+  <div className="App">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/categories" element={<Categories />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </div>
 );
 
 export default App;
---
+++ src/components/Categories.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Styles from '@/styles/Categories.module.scss';
+
+function Categories() {
+  return (
+    <div className={Styles.categories}>
+      Categories
+    </div>
+  );
+}
+
+export default Categories;
