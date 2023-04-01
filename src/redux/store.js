ad8d0676fbaf064266f783ee3aef40ab3d11339d
Precious Betine <preciousbetine@gmail.com>
2023-04-01 09:34:07 +0100

    Change createStore to configureStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,10 @@
-import { createStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import categoriesReducer from '@/redux/categories/categoriesSlice';
 import booksReducer from '@/redux/books/booksSlice';
 
-const store = createStore({
+export default configureStore({
   reducer: {
     categories: categoriesReducer,
     books: booksReducer,
   },
 });
-
-export default store;
